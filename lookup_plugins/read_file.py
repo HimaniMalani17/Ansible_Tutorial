64fb3c6fd67f0f81a655293cb56a7a0111a23f58
HimaniMalani17 <himani.malani@paroscale.com>
2025-06-12 17:47:05 +0530

    Add files via upload

--- lookup_plugins/read_file.py
+++ lookup_plugins/read_file.py
@@ -1,1 +1,10 @@
+import sys
 
+def read_file(file_path):
+    with open (file_path, 'r') as file:
+        return file.read()
+
+if __name__ == "__main__":
+    file_path = "/home/paroscale/myapp/hello.txt"
+    content = read_file(file_path)
+    print(content)
